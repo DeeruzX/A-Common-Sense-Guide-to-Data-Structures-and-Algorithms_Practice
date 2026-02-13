ad5d0caf5a676cbd26593b19a1af193b469d1663
DeeruzX <jehadcoder@gmail.com>
2026-02-13 13:02:20 +0300

    Implement delete and display methods in TreeNode
    Added delete and display methods to TreeNode class, along with lift helper method for node deletion.

--- Binary_Trees.py
+++ Binary_Trees.py
@@ -1,5 +1,7 @@
+#I have to review TreeNode
+
 class TreeNode:
-    def __init__(self,val,left=None,right=None):
+    def __init__(self, val, left=None, right=None):
         self.value = val
         self.leftChild = left
         self.rightChild = right
@@ -24,8 +26,51 @@ class TreeNode:
             else:
                 self.insert(value, node.rightChild)
 
+    def delete(self, valueToDelete, node):
+        if node is None:
+            return None
+        elif valueToDelete < node.value:
+            node.leftChild = self.delete(valueToDelete, node.leftChild)
+            return node
+        elif valueToDelete > node.value:
+            node.rightChild = self.delete(valueToDelete, node.rightChild)
+            return node
+        elif valueToDelete == node.value:
+            if node.leftChild is None:
+                return node.rightChild
+            elif node.rightChild is None:
+                return node.leftChild
+            else:
+                node.rightChild = self.lift(node.rightChild, node)
+                return node
+
+    def lift(self, node, nodeToDelete):
+        if node.leftChild:
+            node.leftChild = self.lift(node.leftChild, nodeToDelete)
+            return node
+        else:
+            nodeToDelete.value = node.value
+            return node.rightChild
+
+    def display(self, node=None, level=0):
+        if node is None:
+            node = self
+        if node.rightChild:
+            self.display(node.rightChild, level + 1)
+        print('    ' * level + str(node.value))
+        if node.leftChild:
+            self.display(node.leftChild, level + 1)
+
 
 node_1 = TreeNode(10)
 node_2 = TreeNode(20)
 root = TreeNode(15, node_1, node_2)
+
+node_1.leftChild = TreeNode(5)
+node_1.rightChild = TreeNode(12)
+
+node_2.leftChild = TreeNode(17)
+node_2.rightChild = TreeNode(25)
+
+root.display()
 
